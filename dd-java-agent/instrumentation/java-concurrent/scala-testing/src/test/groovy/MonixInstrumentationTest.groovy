aad60f847c4d629310e6d9bfe7145586c662c2e2
Michael Pigg <mpigg@starbucks.com>
2020-06-01 15:11:35 -0400

    Add executor and nested tests

--- dd-java-agent/instrumentation/java-concurrent/scala-testing/src/test/groovy/MonixInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/java-concurrent/scala-testing/src/test/groovy/MonixInstrumentationTest.groovy
@@ -46,6 +46,40 @@ class MonixInstrumentationTest extends AgentTestRunner {
     findSpan(trace, "trace-async-task")
   }
 
+  def "test across schedulers"() {
+    setup:
+    ScalaMonixTests monixTest = new ScalaMonixTests()
+    int expectedNumberOfSpans = monixTest.traceAcrossExecutors()
+    TEST_WRITER.waitForTraces(1)
+    List<DDSpan> trace = TEST_WRITER.get(0)
+
+    expect:
+    trace.size() == expectedNumberOfSpans
+    trace[0].resourceName == "ScalaMonixTests.traceAcrossExecutors"
+    findSpan(trace, "global-1")
+    findSpan(trace, "global-2")
+    findSpan(trace, "io-1")
+
+  }
+
+  def "test nested spans"() {
+    setup:
+    ScalaMonixTests monixTest = new ScalaMonixTests()
+    int expectedNumberOfSpans = monixTest.traceNestedSpans()
+    TEST_WRITER.waitForTraces(1)
+    List<DDSpan> trace = TEST_WRITER.get(0)
+
+    expect:
+    trace.size() == expectedNumberOfSpans
+    trace[0].resourceName == "ScalaMonixTests.traceNestedSpans"
+    DDSpan span2 = findSpan(trace, "nested-span-2")
+    DDSpan span1 = findSpan(trace, "nested-span-1")
+    DDSpan span0 = findSpan(trace, "nested-span-0")
+    span0.parentId == span1.spanId
+    span1.parentId == span2.spanId
+    span2.parentId == trace[0].spanId
+  }
+
   private DDSpan findSpan(List<DDSpan> trace, String opName) {
     for (DDSpan span : trace) {
       if (span.getOperationName() == opName) {
--- dd-java-agent/instrumentation/java-concurrent/scala-testing/src/test/scala/ScalaMonixTests.scala
+++ dd-java-agent/instrumentation/java-concurrent/scala-testing/src/test/scala/ScalaMonixTests.scala
@@ -2,13 +2,19 @@ import datadog.trace.api.Trace
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan
 import monix.eval.Task
 import monix.execution.Callback
-import monix.execution.Scheduler.Implicits.global
-
+import monix.execution.Scheduler
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Promise}
 
 
 class ScalaMonixTests {
+
+  val globalScheduler = Scheduler.global
+  val ioScheduler = Scheduler.io("scala-monix-tests")
+
+  def shutdown(): Unit = {
+    ioScheduler.shutdown()
+  }
 
   @Trace
   def tracedChild(opName: String): Unit = {
@@ -18,7 +24,7 @@ class ScalaMonixTests {
   @Trace
   def traceTask: Int = {
     val f = Task.eval(tracedChild("simple-task"))
-    Await.result(f.runToFuture, 5.seconds)
+    Await.result(f.runToFuture(Scheduler.global), 5.seconds)
     2
   }
 
@@ -28,7 +34,7 @@ class ScalaMonixTests {
       t <- Task.eval(tracedChild("for-task-1"))
       u <- Task.eval(tracedChild("for-task-2"))
     } yield 3
-    Await.result(f.runToFuture, 5.seconds)
+    Await.result(f.runToFuture(Scheduler.global), 5.seconds)
   }
 
   @Trace
@@ -36,8 +42,35 @@ class ScalaMonixTests {
     val p = Promise[Int]()
     val f = for {
       t <- Task.eval(tracedChild("trace-async-task"))
-    } yield  2
-    f.runAsync(Callback.fromPromise(p))
+    } yield 2
+    f.runAsync(Callback.fromPromise(p))(Scheduler.global)
     Await.result(p.future, 5.seconds)
+  }
+
+  @Trace
+  def traceAcrossExecutors: Int = {
+    implicit val sc = globalScheduler
+    val f = for {
+      t <- Task.eval(tracedChild("global-1"))
+      u <- Task.eval(tracedChild("io-1")).executeOn(ioScheduler)
+      v <- Task.eval(tracedChild("global-2"))
+    } yield 4
+    Await.result(f.runToFuture, 5.seconds)
+  }
+
+  @Trace
+  def traceNestedSpans: Int = {
+    implicit val sc = globalScheduler
+    val t = Task.eval(tracedChild("nested-span", 2))
+    Await.result(t.runToFuture, 5.seconds)
+    4
+  }
+
+  @Trace
+  def tracedChild(opName: String, level: Int): Unit = {
+    activeSpan().setSpanName(s"$opName-$level")
+    if (level > 0) {
+      tracedChild(opName, level - 1)
+    }
   }
 }
